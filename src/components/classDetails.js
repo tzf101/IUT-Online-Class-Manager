cfc6879b68f89640c7c09d9b84e03bee22ebe2d9
Tariquzzaman-faisal <tariquzzamanfaisal1964@gmail.com>
2021-04-18 23:14:48 +0600

    added class routine in JSON format in src/components/classDetails

--- src/components/classDetails.js
+++ src/components/classDetails.js
@@ -1,25 +1,377 @@
 const links = [
+    //Sunday
+    [
+        //Class Finished for Sunday    
     {
-        class: "Test Class1",
-        start: "11:00",
-        link: "www.testclass1.com"
-    },
-
-    {
-        class: "Test Class2",
-        start: "12:00",
-        link: "www.testclass2.com"
-    },
-    {
-        class: "Test Class3",
-        start: "13:00",
-        link: "www.testclass3.com"
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Sunday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
     },
     {
-        class: "Test Class4",
-        start: "14:00",
-        link: "www.testclass4.com"
-    }
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Sunday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Sunday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Sunday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Sunday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Sunday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Sunday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    }],
+    //Monday
+    [{
+        class: "CSE 4303 - Sabbir Ahmed",
+        end: 1150,
+        week: "Monday",
+        duration: "10.50 - 11.50 am",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "MATH 4341 - Mohsinul Kabir Shanto",
+        end: 1300,
+        week: "Monday",
+        duration: "12:00 - 1:00pm",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "EEE 4383",
+        end: 1430,
+        week: "Monday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+        //Class Finished for Monday
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Monday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Monday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Monday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Monday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    }],
+    //Tuesday
+    [{
+        class: "CSE 4305 - Md. Hamjajul Ashmafee",
+        end: 1040,
+        week: "Tuesday",
+        duration: "9:40 - 10:40 am",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "CSE 4303 - Sabbir Ahmed",
+        end: 1150,
+        week: "Tuesday",
+        duration: "12:00 - 1:00pm",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "CSE 4307 - Abu Raihan Mostofa Kamal",
+        end: 1300,
+        week: "Tuesday",
+        duration: "12:00 - 1:00 pm",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "EEE 4384 - (1A/1B)",
+        end: 1540,
+        week: "Tuesday",
+        duration: "1:30 - 3:40 pm",
+        link: "https://codeforces.com/"
+    },
+        //Class Finished for Tuesday
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Tuesday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Tuesday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Tuesday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    }],
+    //Wednesday
+    [{
+        class: "CSE 4304 - 1A (SA, L_3)",
+        end: 1150,
+        week: "Wednesday",
+        duration: "9:40 - 11:50 am",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "CSE 4304 - 1B (AJ, MHA)",
+        end: 1150,
+        week: "Wednesday",
+        duration: "12:00 - 1:00pm",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "CSE 4307 - Abu Raihan Mostofa Kamal",
+        end: 1300,
+        week: "Wednesday",
+        duration: "12:00 - 1:00 pm",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "EEE 4383",
+        end: 1430,
+        week: "Wednesday",
+        duration: "1:30 - 2:40 pm",
+        link: "https://codeforces.com/"
+    },
+        //Class Finished for Wednesday
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Wednesday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Wednesday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Wednesday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Wednesday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    }],
+    //Thursday
+    [{
+        class: "CSE 4300 - Md. Hamjajul Ashmafee ",
+        end: 1040,
+        week: "Thursday",
+        duration: "9:40 - 10:40 am",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "CSE 4301 -  Faisal Hussain",
+        end: 1300,
+        week: "Thursday",
+        duration: "12:00 - 1:00pm",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "CSE 4302 - 1A (FH,L_7)",
+        end: 1650,
+        week: "Thursday",
+        duration: "2:40 - 4:50 pm",
+        link: "https://codeforces.com/"
+    },
+        //Class Finished for Thursday
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Thursday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Thursday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Thursday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Thursday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    }],
+    //Friday
+    [{
+        class: "CSE 4304 - 1B (SA, SI)",
+        end: 1150,
+        week: "Friday",
+        duration: "9:40 - 11:50 am",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "CSE 4304 -  1A (AJ, MHA)",
+        end: 1150,
+        week: "Friday",
+        duration: "9:40 - 11:50 am",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "MATH 4341 - Mohsinul Kabir Shanto",
+        end: 1300,
+        week: "Friday",
+        duration: "12:00 - 1:00 pm",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "CSE 4301 - Faisal Hussain",
+        end: 1430,
+        week: "Friday",
+        duration: "1:30 - 2:30 pm",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "CSE 4302 - 1B (FH, L_7)",
+        end: 1650,
+        week: "Friday",
+        duration: "2:40 - 4:50 pm",
+        link: "https://codeforces.com/"
+    },
+        //Class Finished for Thursday
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Friday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Friday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Friday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    }],
+    //Saturday
+    [
+        //Class Finished for Saturday    
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Saturday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Saturday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Saturday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Saturday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Saturday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Saturday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    },
+    {
+        class: "No Class today! Solve some problems at codeforces!",
+        end: 2359,
+        week: "Saturday",
+        duration: "N/A",
+        link: "https://codeforces.com/"
+    }]
 ];
 
 export default links;
